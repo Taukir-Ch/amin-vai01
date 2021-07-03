040bc4299108e1f1a6590ed21bc1bd980519529f
Taukir-Ch <www.tukir.ts1987@gmail.com>
2021-07-03 13:35:23 +0600

    5nd

--- assets/js/main.js
+++ assets/js/main.js
@@ -148,4 +148,24 @@ $.scrollUp({
 new WOW().init();
 
 
+$('.qtybutton').on('click', function () {
+	var $button = $(this);
+	var oldValue = $button.parent().find('input').val();
+	if ($button.hasClass('inc')) {
+		var newVal = parseFloat(oldValue) + 1;
+	} else {
+		// Don't allow decrementing below zero
+		if (oldValue > 1) {
+			var newVal = parseFloat(oldValue) - 1;
+		} else {
+			newVal = 1;
+		}
+	}
+	$button.parent().find('input').val(newVal);
+});
+
+
+
 })(jQuery);
+
+
